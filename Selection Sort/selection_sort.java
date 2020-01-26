0afcfae90eb0064d5538017b836821c3d813ca85
mnislam01 <mnazrul.c@example.com>
2020-01-26 22:49:11 +0600

    Selection Sort

---
+++ Selection Sort/selection_sort.java
@@ -0,0 +1,36 @@
+class selection_sort{
+    public static void main(String args[]){
+        int array[] = {23,7,2,8,43,6,78,2,7890,7,235346,23};
+
+        System.out.println("Before sorting...");
+        ShowArray(array);
+        array = InsersionSort(array);
+        System.out.println("After sorting...");
+        ShowArray(array);
+    }
+
+    public static int[] InsersionSort(int list_of_numbers[]){
+        int i, j, smallest, temp;
+        for (i=0; i < list_of_numbers.length; i++){
+            smallest = i;
+            for(j=i+1; j < list_of_numbers.length; j++){
+                if (list_of_numbers[j] < list_of_numbers[smallest]){
+                    smallest = j;
+                }
+            }
+            if (smallest != i) {
+                temp = list_of_numbers[i];
+                list_of_numbers[i] = list_of_numbers[smallest];
+                list_of_numbers[smallest] = temp;
+            }
+        }
+        return list_of_numbers;
+    }
+
+    public static void ShowArray(int arr[]){
+        for (int i=0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
